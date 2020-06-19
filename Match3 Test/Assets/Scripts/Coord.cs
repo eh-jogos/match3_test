ef98f14eca1953225ce2569e8a87672cd89df64a
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-06-19 09:07:44 -0300

    Add Main Scene with Coord Class and WIP Match3 Class

---
+++ Match3 Test/Assets/Scripts/Coord.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Coord
+{
+	public int x;
+	public int y;
+
+	public Coord(int newX, int newY)
+	{
+		x = newX;
+		y = newY;
+	}
+
+	public Vector2 ToVector()
+	{
+		return new Vector2(x, y);
+	}
+
+	public bool Equals(Coord reference)
+	{
+		return (x == reference.x && y == reference.y);
+	}
+
+	public void  mult(int factor)
+	{
+		x *= factor; 
+		y *= factor;
+	}
+
+	public void add(Coord coord)
+	{
+		x += coord.x;
+		y += coord.y;
+	}
+
+	public static Coord fromVector(Vector2 vector)
+	{
+		return new Coord((int)vector.x, (int)vector.y);
+	}
+	
+	public static Coord fromVector(Vector3 vector)
+	{
+		return new Coord((int)vector.x, (int)vector.y);
+	}
+
+	public static Coord mult(Coord coord, int factor)
+	{
+		return new Coord(coord.x * factor, coord.y * factor);
+	}
+
+	public static Coord add(Coord coord1, Coord coord2)
+	{
+		return new Coord(coord1.x + coord2.x, coord1.y + coord2.y);
+	}
+
+	public static Coord clone(Coord coord)
+	{
+		return new Coord(coord.x, coord.y);
+	}
+	
+	public static Coord zero
+	{
+		get{ return new Coord(0, 0); }
+	}
+
+	public static Coord one
+	{
+		get{ return new Coord(1, 1); }
+	}
+
+	public static Coord up
+	{
+		get{ return new Coord(0, 1); }
+	}
+
+	public static Coord left
+	{
+		get{ return new Coord(-1, 0); }
+	}
+
+	public static Coord right
+	{
+		get{ return new Coord(1, 0); }
+	}
+
+	public static Coord down
+	{
+		get{ return new Coord(0, -1); }
+	}
+}
---
+++ Match3 Test/Assets/Scripts/Match3.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match3 : MonoBehaviour
+{
+
+	[SerializeField] Sprite[] pieces;
+
+	int width = 9;
+	int height = 14;
+	Node[,] board;
+
+	System.Random random;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	void StartGame()
+	{
+		string seed = getRandomSeed();
+		random = new System.Random(seed.GetHashCode());
+
+		InitializeBoard();
+	}
+
+	void InitializeBoard()
+	{
+		board = new Node[width, height];
+		for (int line = 0; line < height; line++)
+		{
+			for (int column = 0; column < width; column++)
+			{
+
+				board[column, line] = new Node(fillPieces(), new Coord(column, line));
+			}
+		}
+	}
+
+	void VerifyBoard()
+	{
+		for (int column = 0; column < width; column++)
+		{
+			for (int line = 0; line < height; line++)
+			{
+				Coord coord = new Coord(column, line);
+				int type = getTypeAtCoord(coord);
+			}
+		}
+	}
+
+	int fillPieces()
+	{
+		int type = 1; // We don't want it to be empty, so we don't start at 0
+		type = (random.Next(0,100) / (100 / pieces.Length)) + 1;
+		return type;
+	}
+
+	int getTypeAtCoord(Coord coord)
+	{
+		return board[coord.x, coord.y]; 
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+
+	string getRandomSeed()
+	{
+		string seed = "";
+		string acceptableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*()^";
+		for (int i = 0; i < 20; i++)
+		{
+			seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+		}
+		return seed;
+	}
+}
+
+[System.Serializable]
+public class Node
+{
+	// Represents the type of "gem" that is in this node
+	// -1 = hole
+	// 0 = empty
+	// 1 = milk
+	// 2 = apple
+	// 3 = orange
+	// 4 = bread 
+	// 5 = brocolis
+	// 6 = conconut
+	// 7 = start
+	public int type; 
+	public Coord index;
+
+	public Node(int value, Coord coord)
+	{		
+		type = value;
+		index = coord;
+	}
+}
